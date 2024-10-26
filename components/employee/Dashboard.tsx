08f304aff7e60a08eafe7fc87d21a108d8a49ca3
Kencho Wangdi <kenchowangdi936@gmail.com>
2024-10-26 16:03:37 +0600

    cors in check out api fixed

--- components/employee/Dashboard.tsx
+++ components/employee/Dashboard.tsx
@@ -194,7 +194,7 @@ const Dashboard = () => {
         return;
       }
 
-      const response = await fetch("http://localhost:3000/api/checkout", {
+      const response = await fetch("/api/checkout", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
